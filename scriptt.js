34ed6fe0c50e313452f137ba5228bf020b30f181
Tehreemfatima124 <tehreem.12dev@gmail.com>
2024-10-14 12:54:06 +0500

    rest operator

---
+++ scriptt.js
@@ -0,0 +1,5 @@
+function tripPlain(...trip){
+    console.log("trip items:",trip)   
+    
+}
+    tripPlain("Ali said:",'I will bring chips and drinks',"Ahmad said:",'I will bring cake',"fatima:",'I will bring Ludo');
